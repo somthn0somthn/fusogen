0c88f7fff923b36e7d9c7826c8886d8f209c1ec9
somthn0somthn <lpmaho@gmail.com>
2024-09-19 10:42:24 +0900

    initial setup

--- anchor/programs/fusogen/src/lib.rs
+++ anchor/programs/fusogen/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Mint, TokenAccount, Transfer, Token, MintTo};
 
 declare_id!("8u5DoSAV7cZxQAPYumVRZCJeYoijkkjCHsGgC6gKyp4m");
 
@@ -6,11 +7,41 @@ declare_id!("8u5DoSAV7cZxQAPYumVRZCJeYoijkkjCHsGgC6gKyp4m");
 pub mod fusogen {
     use super::*;
 
-    pub fn greet(_ctx: Context<Initialize>) -> Result<()> {
-        msg!("GM!");
+    pub fn initialize_mint(ctx: Context<InitializeMint>) -> Result<()> {
+        let mint_account = &mut ctx.accounts.mint_account;        
+        let exchange_ratio: u64 = 100;
+        mint_account.exchange_ratio = exchange_ratio;
+        mint_account.mint = ctx.accounts.mint.key();
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializeMint<'info> {
+    #[account(init, payer = user, space = 8 + 8 + 32)]
+    pub mint_account: Account<'info, MintAccount>,
+    #[account(mut)]
+    pub mint: Account<'info, Mint>,
+/*     #[account(init, payer = user, token::mint = mint, token::authority = user)]
+    pub vault_a: Account<'info, TokenAccount>,
+    #[account(init, payer = user, token::mint = mint, token::authority = user)]
+    pub vault_b: Account<'info, TokenAccount>, */
+    #[account(mut)]
+    pub user: Signer<'info>,
+    
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>,
+    pub token_program: Program<'info, Token>,    
+}
+
+#[account]
+pub struct MintAccount {
+    pub exchange_ratio: u64,
+    pub mint: Pubkey, 
+/*     pub vault_a: Pubkey,
+    pub vault_b: Pubkey, */
+}
+
+
+//CONT:: Commit this, make a new branch and Continue thinking about how vault_a and vault_b fit into this code
--- anchor/tests/fusogen.spec.ts
+++ anchor/tests/fusogen.spec.ts
@@ -1,16 +1,62 @@
 import * as anchor from '@coral-xyz/anchor';
 import { Program } from '@coral-xyz/anchor';
 import { Fusogen } from '../target/types/fusogen';
+import {
+  LAMPORTS_PER_SOL,
+  SystemProgram,
+  Keypair,
+  PublicKey,
+} from '@solana/web3.js';
+import {
+  TOKEN_PROGRAM_ID,
+  createMint,
+  createAccount,
+  mintTo
+} from '@solana/spl-token';
+
 
 describe('fusogen', () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.local();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.Fusogen as Program<Fusogen>;
+  const user = Keypair.generate();
+  const mintAccount = Keypair.generate();
 
-  it('should run the program', async () => {
+  let newTokenMint: PublicKey;
+  let tokenAccountA: PublicKey;
+  let tokenAccountB: PublicKey;
+
+  beforeAll(async () => {
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(
+        user.publicKey,
+        10 * LAMPORTS_PER_SOL
+      )
+    );
+
+    newTokenMint = await createMint(
+      provider.connection,
+      user,
+      user.publicKey,
+      null,
+      250
+    )
+  })
+
+  it('Initializes the new token Mint ', async () => {
     // Add your test here.
-    const tx = await program.methods.greet().rpc();
+    const tx = await program.methods
+      .initializeMint()
+      .accounts({
+        mintAccount: mintAccount.publicKey,
+        mint: newTokenMint,
+        user: user.publicKey,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([user, mintAccount])
+      .rpc();
     console.log('Your transaction signature', tx);
   });
 });
